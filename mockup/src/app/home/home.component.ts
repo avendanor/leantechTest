b17035cb4c950004ad358225716cd9c381b0085e
Andrés Avendaño <andresavendano@MacBook-Pro-de-Andres.local>
2019-09-14 15:37:32 -0500

    detalles vista principal

--- mockup/src/app/home/home.component.ts
+++ mockup/src/app/home/home.component.ts
@@ -9,15 +9,22 @@ import { Http } from '@angular/http';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  posts: any[]; 
+  posts: any[];
+  characters = [
+    ['Friday 10th 2:28pm',"Houston, TX, 33619","Atlanta, GA, 30123","250 USD"],
+    ['Satuday 11th 8:28am',"Houston, TX, 33619","Atlanta, GA, 30123","150 USD"],
+    ['Saturday 11th 2:28pm',"Houston, TX, 33619","Atlanta, GA, 30123","250 USD"],
+    ['Sunday 12th 2:28pm',"Houston, TX, 33619","Atlanta, GA, 30123","280 USD"],
+    ['Monday 13th 10:28pm',"Houston, TX, 33619","Atlanta, GA, 30123","130 USD"],
+    ['Monday 13th 2:28pm',"Houston, TX, 33619","Atlanta, GA, 30123","150 USD"]
+  ] 
   tcode : string;
   constructor(http: Http) { 
+    //intento de traer la info desde un json
     http.get('https://jsonplaceholder.typicode.com/users')  
     .subscribe(response => {  
       this.posts = response.json();
-      console.log(typeof(this.posts));  
     });
-    
   }
   filtrar() : void {
     console.log(this.tcode);
