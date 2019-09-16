df949c81b09a9e192a864212ee6baf55627224c7
Andrés Avendaño <andresavendano@MacBook-Pro-de-Andres.local>
2019-09-15 22:08:53 -0500

    settings finished

--- mockup/src/app/settings/settings.component.ts
+++ mockup/src/app/settings/settings.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+      } else {
+      dropdownContent.style.display = "block";
+      }
+      });
+    }
   }
 
 }
